4e2ca69a32dd4493449a49da87ab8f87b8494aae
Petr <kim.pyoter2017@yandex.ru>
2023-01-03 22:48:41 +0300

    fixlayout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,47 +9,47 @@ export const Navbar = () => {
             setBtn("list-container")
         }
     }
-  return (
-    <nav>
-    <div className={btn}>
-        <button className="more-button" aria-label="Menu Button" onClick={clickMenu}>
-            <div className="menu-icon-wrapper">
-                <div className="menu-icon-line half first"></div>
-                <div className="menu-icon-line"></div>
-                <div className="menu-icon-line half last"></div>
+    return (
+        <nav>
+            <div className={btn}>
+                <button className="more-button" aria-label="Menu Button" onClick={clickMenu}>
+                    <div className="menu-icon-wrapper">
+                        <div className="menu-icon-line half first"></div>
+                        <div className="menu-icon-line"></div>
+                        <div className="menu-icon-line half last"></div>
+                    </div>
+                </button>
+                <ul className="more-button-list">
+                    <li className="more-button-list-item">
+                        <span>Главная</span>
+                    </li>
+                   
+                    <a href="https://github.com/Petr-lover-of-justice/ShopPhone">
+                    <li className="more-button-list-item">
+                        <span>Код проекта</span>
+                    </li>
+                    </a>
+                    <li className="more-button-list-item">
+                        <span>Корзина</span>
+                    </li>
+                    <li className="more-button-list-item">
+                        <span>Просто</span>
+                    </li>
+                </ul>
             </div>
-        </button>
-        <ul className="more-button-list">
-            <li className="more-button-list-item">
-                <span>Главная</span>
-            </li>
-            <li className="more-button-list-item">
-                <span>Код проекта</span>
-            </li>
-            <li className="more-button-list-item">
-                <span>Корзина</span>
-            </li>
-            <li className="more-button-list-item">
-                <span>Просто</span>
-            </li>
-        </ul>
-    </div>
-    <div className="socials">
-        <a className="social-link" href="https://twitter.com/aybukeceylan" target="_top">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-            </svg>
-        </a>
-        <a className="social-link" href="https://www.linkedin.com/in/ayb%C3%BCkeceylan/" target="_top">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect x="2" y="9" width="4" height="12" /><circle cx="4" cy="4" r="2" /></svg>
-        </a>
-    </div>
-    <div className="menu">
-       <a href="#"><span>Главная</span></a> 
-       <a href="#"><span>Код проекта</span></a> 
-       <a href="#"><span>Корзина</span></a> 
-       <a href="#"><span>Просто</span></a> 
-    </div>
-</nav>
-  )
+            <div className="socials">
+                <a className="social-link" href="https://github.com/Petr-lover-of-justice/ShopPhone" target="_top">
+                    <svg width="34" height="34" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" transform="scale(64)" fill="#1B1F23" />
+                    </svg>
+                </a>
+            </div>
+            <div className="menu">
+                <a href="#"><span>Главная</span></a>
+                <a href="https://github.com/Petr-lover-of-justice/ShopPhone"><span>Код проекта</span></a>
+                <a href="#"><span>Корзина</span></a>
+                <a href="#"><span>Просто</span></a>
+            </div>
+        </nav>
+    )
 }
