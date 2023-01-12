ff5e6c78b5638c4a8d509991644ca609f6fe50e9
Petr <70943698+Petr-lover-of-justice@users.noreply.github.com>
2023-01-12 15:50:14 +0300

    Update Registration.tsx

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
-// import { Login } from '../components/Login'
 
 export const Registration = () => {
   return (
     <div className="registration">
-      {/* <!--       <div className="registration__imgLogo"> <img src="https://portal.mi-room.ru/wp-content/uploads/2018/08/mitu-work_hard_dream_big.jpg" alt="ff" /></div>
-      <div className="registration__enter">
-      <Login /> 
-      </div> --> */}
     </div>
   )
 }
