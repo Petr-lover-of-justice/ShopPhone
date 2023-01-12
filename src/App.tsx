ed83c1fd1f6219e921426d4e6a4edaf051e57ebe
Petr <70943698+Petr-lover-of-justice@users.noreply.github.com>
2023-01-12 17:50:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path="/ShopPhone/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route
           path="cart"
           element={
